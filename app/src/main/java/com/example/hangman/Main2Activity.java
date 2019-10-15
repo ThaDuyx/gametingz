02dc5a8d488cd1689c66df8b15eaab599a174843
Duyx <sandersen98@live.dk>
2019-10-15 21:34:57 +0200

    hej

--- app/src/main/java/com/example/hangman/Main2Activity.java
+++ app/src/main/java/com/example/hangman/Main2Activity.java
@@ -1,20 +1,22 @@
 package com.example.hangman;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
 
     GameLogic logic = new GameLogic();
     Button guessBtn;
+    Button resetBtn;
     EditText editGuess;
     TextView wordFrame;
     TextView guessFrame;
+    ImageView hangManpic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +27,14 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
         wordFrame = findViewById(R.id.wordFrame);
         guessFrame = findViewById(R.id.guessFrame);
         guessBtn = findViewById(R.id.guessBtn);
+        resetBtn = findViewById(R.id.resetBtn);
         editGuess = findViewById(R.id.editGuess);
+        hangManpic = findViewById(R.id.hangmanPic);
 
         wordFrame.setText(logic.getSynligtOrd());
         guessBtn.setOnClickListener(this);
+        resetBtn.setOnClickListener(this);
+        resetBtn.setVisibility(View.GONE);
         guessFrame.setText("");
         logic.logStatus();
 
@@ -38,27 +44,64 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onClick(View v) {
 
+        if( v == resetBtn && logic.erSpilletSlut()){
+            logic.nulstil();
+            resetBtn.setVisibility(View.GONE);
+            wordFrame.setText(logic.getSynligtOrd());
+            hangManpic.setImageResource(R.drawable.galge);
+            guessFrame.setText("Guess the new word");
+        }
+
         String guess = editGuess.getText().toString();
         if (guess.length() != 1) {
             editGuess.setError("1 letter at a time");
             return;
         }
+
         logic.gætBogstav(guess);
         editGuess.setText("");
 
-        reset();
+        if(logic.getAntalForkerteBogstaver() == 0){
+            hangManpic.setImageResource(R.drawable.galge);
+        } else if(logic.getAntalForkerteBogstaver() == 1){
+            hangManpic.setImageResource(R.drawable.forkert1);
+        } else if(logic.getAntalForkerteBogstaver() == 2){
+            hangManpic.setImageResource(R.drawable.forkert2);
+        } else if(logic.getAntalForkerteBogstaver() == 3){
+            hangManpic.setImageResource(R.drawable.forkert3);
+        } else if(logic.getAntalForkerteBogstaver() == 4){
+            hangManpic.setImageResource(R.drawable.forkert4);
+        } else if(logic.getAntalForkerteBogstaver() == 5){
+            hangManpic.setImageResource(R.drawable.forkert5);
+        } else if(logic.getAntalForkerteBogstaver() == 6){
+            hangManpic.setImageResource(R.drawable.forkert6);
+        }
+
+       /* if(logic.erSpilletSlut()){
+            logic.nulstil();
+        }
+        if(guessBtn.getText().toString() == "Play"){
+            logic.nulstil();
+            guessBtn.setText("Guess");
+        }*/
+
+        updateScreen();
     }
 
-    private void reset(){
+    private void updateScreen(){
 
         wordFrame.setText(logic.getSynligtOrd());
         guessFrame.setText("\n\nDu har " + logic.getAntalForkerteBogstaver() + " forkerte:" + logic.getBrugteBogstaver());
 
         if (logic.erSpilletVundet()) {
             guessFrame.setText("\nDu har vundet");
+            //guessBtn.setText("Play");
+            resetBtn.setVisibility(View.VISIBLE);
         }
         if (logic.erSpilletTabt()) {
             wordFrame.setText("Du har tabt, ordet var : " + logic.getOrdet());
+            //guessBtn.setText("Play");
+            resetBtn.setVisibility(View.VISIBLE);
         }
 
     }
--- app/src/main/java/com/example/hangman/MainActivity.java
+++ app/src/main/java/com/example/hangman/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.hangman;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
